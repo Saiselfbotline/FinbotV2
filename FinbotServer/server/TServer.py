ab73b98401c31215215d8db8817f3a58eb42e99d
ALFINONH <39095075+ALFINONH@users.noreply.github.com>
2018-07-11 02:26:58 +0700

    Update TServer.py

--- FinbotServer/server/TServer.py
+++ FinbotServer/server/TServer.py
@@ -1,33 +1,10 @@
-#
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements. See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership. The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License. You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied. See the License for the
-# specific language governing permissions and limitations
-# under the License.
-#
-
 from six.moves import queue
 import logging
 import os
 import threading
-
 from Thrift.protocol import TBinaryProtocol
 from Thrift.transport import TTransport
-
 logger = logging.getLogger(__name__)
-
-
 class TServer(object):
     """Base interface for a server, which must have a serve() method.
 
@@ -62,7 +39,6 @@ class TServer(object):
 
     def serve(self):
         pass
-
 
 class TSimpleServer(TServer):
     """Simple single-threaded server that just pumps around one transport."""
